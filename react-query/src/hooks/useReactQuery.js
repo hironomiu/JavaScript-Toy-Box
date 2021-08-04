707ab50ae83d1e8833d83c7662c20e27d236a5ad
hironomiu <hironomiu@gmail.com>
2021-08-04 18:49:19 +0900

    commited

--- react-query/src/hooks/useReactQuery.js
+++ react-query/src/hooks/useReactQuery.js
@@ -1,16 +1,15 @@
 import { useQuery } from 'react-query'
 
-const getTasks = async () => {
+const getUsers = async () => {
   const data = await fetch('http://localhost:5000/api/users')
   const json = await data.json()
-  console.log(json)
   return json
 }
 
 export const useQueryTasks = () => {
   return useQuery({
     queryKey: 'users',
-    queryFn: getTasks,
+    queryFn: getUsers,
     cacheTime: 30000,
     staleTime: 30000,
   })
