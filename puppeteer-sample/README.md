d39b89281b27e1627908b139bf46f6a68d4f46ba
hironomiu <hironomiu@gmail.com>
2021-11-17 11:01:52 +0900

    commited

---
+++ puppeteer-sample/README.md
@@ -0,0 +1,16 @@
+# puppeteer
+
+[公式：puppeteer](https://github.com/puppeteer/puppeteer)
+
+## Run
+
+```
+npm start
+```
+
+## SetUp
+
+```
+npm init -y
+npm install puppeteer puppeteer-core
+```
---
+++ puppeteer-sample/main.js
@@ -0,0 +1,29 @@
+const puppeteer = require('puppeteer')
+
+;(async () => {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  try {
+    await page.goto('https://www.google.com/')
+    await page.type('input[name="q"', 'github hironomiu')
+    await page.keyboard.press('Enter')
+    await page.waitForSelector('h3 a', {
+      waitUntil: 'networkidle2',
+      timeout: 2500,
+    })
+    const aTagAll = await page.$$('a')
+    let tagText = []
+    let indicator = ''
+    console.log(aTagAll.length)
+    for (let i = 0; i < aTagAll.length; i++) {
+      // console.log(aTagAll[i])
+    }
+    // aTagAll[]
+    // await page.waitFor(1500)
+    await page.screenshot({ path: './image.png' })
+  } catch (err) {
+    null
+    // console.log(err)
+  }
+  await browser.close()
+})()
