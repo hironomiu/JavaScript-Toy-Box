3128ee030b5a4243d45ee2784b2b1b4f381142c4
hironomiu <hironomiu@gmail.com>
2021-11-19 09:32:00 +0900

    commited

--- redux-counter/src/js/app.js
+++ redux-counter/src/js/app.js
@@ -1,6 +1,19 @@
-import { store, COUNT_INCREMENT, COUNT_DECREMENT } from './modules/redux'
+import {
+  store,
+  COUNT_INCREMENT,
+  COUNT_DECREMENT,
+  COUNT_INPUT_VALUE_INCREMENT,
+  AMOUNT_SET,
+} from './modules/redux'
 
 const $element = document.getElementById('app')
+// eslint-disable-next-line no-unused-vars
+
+let inputAmount = 0
+const updateInput = (e) => {
+  if (e.target.value === '') return
+  inputAmount = e.target.value
+}
 
 const render = () => {
   const { count } = store.getState()
@@ -15,6 +28,10 @@ const render = () => {
     <p style="font-size:x-large;margin:2px 4px;width:40px;text-align:center">${count}</p>
     <button id='increment_button'>increment</button>
   </div>
+  <div style="display:flex;justify-content: center;margin-top:10px;">
+    <input style="text-align:center;" type="text" id='input' value=${inputAmount} />
+    <button id='input_increment_button'>increment</button
+  </div>
   `
 
   const $incrementButton = document.getElementById('increment_button')
@@ -25,6 +42,21 @@ const render = () => {
   const $decrementButton = document.getElementById('decrement_button')
   $decrementButton.addEventListener('click', () => {
     store.dispatch({ type: COUNT_DECREMENT })
+  })
+
+  const $input = document.getElementById('input')
+  $input.focus()
+  $input.addEventListener('input', updateInput)
+
+  const $inputIncrementButton = document.getElementById(
+    'input_increment_button'
+  )
+  $inputIncrementButton.addEventListener('click', () => {
+    store.dispatch({ type: AMOUNT_SET, payload: inputAmount })
+    store.dispatch({
+      type: COUNT_INPUT_VALUE_INCREMENT,
+      payload: parseInt(inputAmount) || 0,
+    })
   })
 }
 
--- redux-counter/src/js/modules/redux.js
+++ redux-counter/src/js/modules/redux.js
@@ -2,6 +2,8 @@ import { createStore, combineReducers } from 'redux'
 
 export const COUNT_INCREMENT = 'count/increment'
 export const COUNT_DECREMENT = 'count/decrement'
+export const COUNT_INPUT_VALUE_INCREMENT = 'count/inputVlueIncrement'
+export const AMOUNT_SET = 'amount/set'
 
 const count = (state = 0, action) => {
   switch (action.type) {
@@ -9,9 +11,24 @@ const count = (state = 0, action) => {
       return state + 1
     case COUNT_DECREMENT:
       return state - 1
+    case COUNT_INPUT_VALUE_INCREMENT:
+      console.log('COUNT_INPUT_VALUE_INCREMENT')
+      return (state += action.payload)
     default:
       return state
   }
 }
 
-export const store = createStore(combineReducers({ count }))
+const amount = (state = 5, action) => {
+  switch (action.type) {
+    case AMOUNT_SET:
+      console.log('AMOUNT_SET:', action.payload)
+      // eslint-disable-next-line no-case-declarations
+      const amount = parseInt(action.payload) || 0
+      return (state = amount)
+    default:
+      return state
+  }
+}
+
+export const store = createStore(combineReducers({ count, amount }))
