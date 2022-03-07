5f726514e160e496e8a86371681549fad902b35a
hironomiu <hironomiu@gmail.com>
2022-03-08 08:12:12 +0900

    commited

--- tips/currying/main.js
+++ tips/currying/main.js
@@ -1,6 +1,20 @@
 console.log('カリー化')
 
 {
+  function add(num1) {
+    return function func1(num2) {
+      return num1 + num2
+    }
+  }
+  console.log(add(1)(2))
+}
+
+{
+  const add = (num1) => (num2) => num1 + num2
+  console.log(add(3)(4))
+}
+
+{
   function add() {
     return function func1(num1) {
       return function func2(num2) {
