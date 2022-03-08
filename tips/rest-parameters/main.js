ec45324fb12e7080030521cecfdc2c5716340e25
hironomiu <hironomiu@gmail.com>
2022-03-08 09:03:47 +0900

    commited

--- tips/rest-parameters/main.js
+++ tips/rest-parameters/main.js
@@ -13,9 +13,14 @@ console.log('残余引数(Rest Parameters)')
     return Array.from({ length }, () => value)
   }
 
+  function concat(val1, val2, val3) {
+    return val1 + val2 + val3
+  }
+
   function call(fn, ...args) {
     return fn(...args)
   }
 
   console.log(call(fill, 10, 'a'))
+  console.log(call(concat, 'hello', ' world', ' !!'))
 }
