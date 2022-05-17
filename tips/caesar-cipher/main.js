3c32d2c4afa5d07dc1234e2557b4052c55d62b7f
hironomiu <hironomiu@gmail.com>
2022-05-17 18:13:41 +0900

    commited

---
+++ tips/caesar-cipher/main.js
@@ -0,0 +1,24 @@
+console.log('Caesar cipher(シーザー暗号)')
+
+const caesarCipher = (str, interval) => {
+  console.log('Input:', str, ' Interval:', interval)
+  const codePointA = 'A'.codePointAt(0)
+  const codePointZ = 'Z'.codePointAt(0)
+  const m = str
+    .split('')
+    .map((char) => {
+      const codePoint = char.codePointAt(0)
+      if (codePoint >= codePointA && codePoint <= codePointZ) {
+        return codePoint + interval - codePointZ > 0
+          ? codePointA + codePoint + interval - codePointZ - 1
+          : codePoint + interval
+      }
+      return codePoint
+    })
+    .map((codePoint) => String.fromCharCode(codePoint))
+    .join('')
+
+  return m
+}
+
+console.log('Output:', caesarCipher('HELLO WORLD ZZ', 3))
